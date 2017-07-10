f8fedd1e969abd27c951ec663de389aff14bfc21
Tushar Tuteja <ttuteja@gmail.com>
2017-07-10 12:56:55 +0530

    uploaded users image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,8 +33,18 @@ class HomeController < ApplicationController
   end
 
   def upload_image
+  	uploaded_file = params[:image]
+  	filename = SecureRandom.hex + "." +uploaded_file.original_filename.split('.')[1]
+  	filepath = Dir.pwd + "/public/uploads/" + filename
+	File.open(filepath,'wb') do |file|
+		file.write(uploaded_file.read())
+	end
+
+	current_user.profile_picture = filename
+	current_user.save!
+  	return redirect_to '/profile'
   end
-  
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710055747) do
+ActiveRecord::Schema.define(version: 20170710070738) do
 
   create_table "follow_mappings", force: :cascade do |t|
     t.integer  "follower_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170710055747) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "profile_picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
