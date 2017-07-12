d703283fcb7bb94b8fd698874e885d1429976f56
Tushar Tuteja <ttuteja@gmail.com>
2017-07-12 13:44:57 +0530

    added infinite scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -35,6 +35,28 @@ $(document).ready(function() {
     });
 
 
+    window.load_more_active = false;
+    window.offset = 10;
+    document.addEventListener('scroll', function(event){
+        
+        if(!window.load_more_active){
+           if(window.scrollMaxY - event.pageY < 50){
+            window.load_more_active = true;
+            $.ajax({
+              url: '/',
+              type: 'GET',
+              dataType: 'script',
+              data: {
+              offset: window.offset,
+              format: 'js'
+              }
+            });
+        } 
+        }
+        
+    });
+
+
 
 
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,28 @@
 class HomeController < ApplicationController
   before_action :authenticate_user! 	
   def index
-  	@tweet = Tweet.new
+
+
+    respond_to do |format|
+      format.html{
+            @tweet = Tweet.new
+            @feed = current_user.feed.limit(10)
+
+      }
+      format.js{
+            offset = params["offset"]
+            if offset
+              offset = offset.to_i
+            else
+              offset = 0
+            end
+
+            @new_offset = offset + 10
+            @show_load_more = offset < current_user.feed.count
+            @feed = current_user.feed.offset(offset).limit(10)
+      }
+    end
+
   end
 
 
